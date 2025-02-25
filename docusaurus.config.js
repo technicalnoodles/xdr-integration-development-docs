cd215e924afc47b16df6a2c5ca3cf553af9d6d54
technicalnoodles <galica9999@gmail.com>
2025-02-25 15:42:48 -0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,23 +62,7 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          editUrl: 'cisco.com',
         },
         theme: {
           customCss: './src/css/custom.css',
