0b6f0563b9ed0c5d3c3e87b78374e58d19037118
technicalnoodles <galica9999@gmail.com>
2025-02-25 23:19:44 -0600

    updated some info and added apis

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,20 +11,20 @@ import ScalarOptions from '@scalar/docusaurus';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: 'Cisco XDR Integration Resources',
+  tagline: 'Create an integration with Cisco XDR',
+  favicon: 'img/cisco-logo-transparent.png',
 
   // Set the production url of your site here
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs/intro',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Cisco', // Usually your GitHub org/user name.
+  projectName: 'Cisco XDR Integration Resources', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -124,10 +124,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'Cisco XDR Dev',
+        title: 'Cisco XDR Integration Resources',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Cisco Logo',
+          src: 'img/cisco-logo-transparent.png',
         },
         items: [
           {
@@ -138,7 +138,7 @@ const config = {
           },
 
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://cisco.com',
             label: 'GitHub',
             position: 'right',
           },
@@ -146,48 +146,8 @@ const config = {
       },
       footer: {
         style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        links: [],
+        copyright: `Copyright © ${new Date().getFullYear()} Cisco Systems.`,
       },
       prism: {
         theme: prismThemes.github,
